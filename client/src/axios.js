72eb7bc6d98347db15f61f6eb1302e0a579ce842
YN1kiforov <yakov-nikiforov.2@mail.ru>
2022-09-07 17:21:56 +0700

    fix

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://online-chat-mern.herokuapp.com/',
+  baseURL: 'https://online-chat-mern.herokuapp.com',
 });
 
 export default instance;
